6648d3b2066ff9c717bc819354ca1d27ecd3e6e1
bfsantos <bfsantos@rd.com.br>
2021-11-07 17:57:29 -0300

    organizando e filtrando return de messagens

--- backend/src/services/GetLastMessagesService.ts
+++ backend/src/services/GetLastMessagesService.ts
@@ -1,8 +1,32 @@
 import { Message } from "@prisma/client";
 import prismaClient from "../prisma"
 
+interface IReturnFilterMessage{
+    id: string;
+    text: string;
+    create_at: Date
+}
+
+interface IInfoMessage {
+    id: string;
+    text: string;
+    create_at: Date;
+}
+
+interface IUserSender {
+    id: string;
+    name: string;
+    github_id: number;
+    avatar_url: string;
+    login: string;
+}
+
+interface IReturnGetMessages {
+    infoMessage: IInfoMessage;
+    userSender: IUserSender;
+}
 class GetLastMessagesService {
-    async execute(): Promise<Message[]> {
+    async execute(): Promise<IReturnGetMessages[]> {
         const messages = await prismaClient.message.findMany({
             take: 6,
             orderBy: {
@@ -12,8 +36,22 @@ class GetLastMessagesService {
                 user: true
             }
         });
-        return messages;
-   }
+        const result: IReturnGetMessages[] = [];
+        messages.map(message => {
+            return result.push({ infoMessage: this.filterMessages(message), userSender: message.user });
+        })
+        console.log(result);
+        return result; 
+    }
+
+    private filterMessages(data: Message): IReturnFilterMessage {
+        return {
+            id: data.id,
+            text: data.text,
+            create_at: data.create_at
+        }
+    }
+
 }
 
 export { GetLastMessagesService }
